2c18b47e38ce22c1ed21a305af98e0ddb134439e
KuramaFoxMax <themaxx858@gmail.com>
2018-09-12 22:15:01 -0500

    Versionv1

--- app/src/main/java/com/example/autonoma/aplicativo/MainActivity.java
+++ app/src/main/java/com/example/autonoma/aplicativo/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton btnMas;
     Button btnGuar;
     EditText txtEMAIL;
-
+//
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
